95de842002be2ca14375c7e7fedf2373e3ab0e13
manoj <manojkumarennam123@gmail.com>
2023-02-24 09:52:29 +0530

    vars

---
+++ 04-vars/var.tf
@@ -0,0 +1,18 @@
+variable "sample" {
+  default = 100
+}
+output "sample" {
+  value = var.sample
+}
+variable "sample1" {
+  default = 100
+}
+output "sample1" {
+  value = var.sample1
+}
+
+variable "cli" {
+}
+output "cli" {
+  value = var.cli
+}
