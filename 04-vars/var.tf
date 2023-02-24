ece228eedb6db585fd3214877fd2b3b45a3796a9
manoj <manojkumarennam123@gmail.com>
2023-02-24 10:01:11 +0530

    vars

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -16,3 +16,29 @@ variable "cli" {
 output "cli" {
   value = var.cli
 }
+variable "input" {}
+
+variable "sample5" {
+  default = "Hello"
+}
+
+variable "sample6" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample7" {
+  default = {
+    string  = "Hello",
+    number  = 100,
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "value sample 5 - ${var.sample5}, first value in list ${var.sample6[4]}, boolean value in map ${var.sample7[boolean]}"
+}
